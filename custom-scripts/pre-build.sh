27790e00725aa146f8b23ba8484cd4222f8ada1b
Willian <willian@DEB9-VBOX>
2019-08-26 20:52:34 -0300

    Alterações para executar no PC de casa

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -2,3 +2,4 @@
 
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+cp $BASE_DIR/../custom-scripts/simple_http_server.py $BASE_DIR/target/root
