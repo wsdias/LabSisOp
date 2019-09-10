e995d457792c8f7b9d342657534849b427b1fb51
Willian <willian@DEB9-VBOX>
2019-09-09 23:15:57 -0300

    Concluído: Tutorial 2.2

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -7,3 +7,8 @@ cp $BASE_DIR/../custom-scripts/S42pythonServer $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S42pythonServer
 
 cp $BASE_DIR/../custom-scripts/simple_http_server.py $BASE_DIR/target/usr/bin
+
+#Compile the syscall_test.c
+BUILDROOT_DIR=$BASE_DIR/..
+COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
+$COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
