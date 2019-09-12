c56d4454cda3c575d5e9bf510081f1616da6d00c
Cazzruan <c.ruan@edu.pucrs.br>
2019-09-12 09:40:26 -0300

    Alterações para novas syscall (processSleepInfo) --COM_ERRO

---
+++ custom-scripts/syscall_sleep_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define SYSCALL_PROCESSINFO	385
+
+int main(int argc, char** argv)
+{  
+    char buf[256];
+    long ret;
+
+    printf("Invoking 'listProcessSleepInfo' system call.\n");
+       
+    ret = syscall(SYSCALL_PROCESSSLEEPINFO, buf, sizeof(buf)); 
+
+	/* Success, show the process info. */       
+    if(ret > 0) printf("%s\n", buf);
+    else printf("System call 'listProcessSleepInfo' did not execute as expected error %d\n", ret);
+        
+    return 0;
+}
