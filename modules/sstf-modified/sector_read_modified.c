c0e4361762a917d6758f8e6e917c25e3c83bc7ca
wsdias <willian.dias@edu.pucrs.br>
2019-10-08 09:35:52 -0300

    T2 em progresso

--- modules/sstf-modified/sector_read_modified.c
+++ modules/sstf-modified/sector_read_modified.c
@@ -8,13 +8,16 @@
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 #define BUFFER_LENGTH 512
 #define DISK_SZ	1073741824
 
 int main(){
 	int ret, fd, pid, i;
-	unsigned int pos;
+    //unsigned int posi[5] = {1261960, 689984, 64968, 994448, 1603288};
+    unsigned int posi[12] = {1,2,3,4,5,6,7,8,9,10,11,12};
+	unsigned int pos, flag, min, max;
 	char buf[BUFFER_LENGTH];
 
 	printf("Starting sector read example...\n");
@@ -32,13 +35,29 @@ int main(){
 
 	strcpy(buf, "hello world!");
 
-	for (i = 0; i < 50; i++){
-		pos = (rand() % (DISK_SZ >> 9));
-		printf("Sector: %lu\n", pos);
-		/* Set position */
-		lseek(fd, pos * 512, SEEK_SET);
-		/* Peform read. */
-		read(fd, buf, 100);
+	for (i = 0; i < 12; i++){
+
+        if (i % 4 == 0){
+
+            if (fork() == 0){
+
+                flag = 1;
+                i += 4;
+                max = i + 3;
+            }
+            else{
+
+                max = i + 3;
+                flag = 0;
+            }
+        }
+
+        if (i == max) i = 12;
+
+		printf("Flag: %lu | Sector: %lu\n", flag, posi[i]);
+
+	    lseek(fd, posi[i] * 512, SEEK_SET);
+	    read(fd, buf, 100);
 	}
 	close(fd);
 
